2d23a9a2f5a958e785ee0555cb70c0557086561d
hjaox <hectorjana.o@gmail.com>
2024-03-20 15:02:27 +0000

    [FEAT] added mongoDB for development and test enviroment

---
+++ server/src/db/connection.ts
@@ -0,0 +1,20 @@
+import mongoose, { connection } from "mongoose";
+import * as dotenv from "dotenv";
+
+const ENV = process.env.NODE_ENV || "development";
+
+dotenv.config({
+    path: `${__dirname}/../../.env.${ENV}`
+});
+
+export default async () => {
+    if (process.env.MONGODBURL) {
+        try {
+            await mongoose.connect(process.env.MONGODBURL)
+        } catch (err) {
+            console.log(err)
+        }
+    } else {
+        console.log("Provide URL for database connection in env file")
+    }
+}
