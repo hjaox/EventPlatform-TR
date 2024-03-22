8011a1c69eafadd72911468225e5b87c0a650513
hjaox <hectorjana.o@gmail.com>
2024-03-22 23:10:26 +0000

    [EDIT] implemented sanitation of query

--- server/src/models/register.model.ts
+++ server/src/models/register.model.ts
@@ -1,3 +1,4 @@
+import { sanitizeFilter } from "mongoose";
 import UserModel from "../mongo/models/user.model";
 import auth from "../utils/firebase/fbAuth";
 import { signUp } from "../utils/firebase/fbFunctions";
@@ -9,11 +10,13 @@ export async function postUser(name: string, email: string, password: string) {
         const userToken = await userCredentials.user.getIdToken();
         const uid = userCredentials.user.uid;
 
-        const newUser = await UserModel.create({ name, email, password, _id: uid });
+        const sanitizedQuery = sanitizeFilter({ name, email, _id: uid });
+
+        const newUser = await UserModel.create(sanitizedQuery);
 
         return { newUser, userToken };
     } catch (err) {
-        if(err === "Sign Up failed") return Promise.reject({status: 400, msg: err})
+        if (err === "Sign Up failed") return Promise.reject({ status: 400, msg: err })
         console.log("Model postUser error", err)
         return Promise.reject(err)
     }
