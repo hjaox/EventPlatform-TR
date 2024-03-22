f40c46db24189c4e57424ed77b3d0f4c903a310e
hjaox <hectorjana.o@gmail.com>
2024-03-22 23:01:32 +0000

    [FEAT] created firebase signIn function

--- server/src/utils/firebase/fbFunctions.ts
+++ server/src/utils/firebase/fbFunctions.ts
@@ -1,12 +1,22 @@
-import { Auth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 export async function signUp(auth: Auth, email: string, password: string) {
     try{
         const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredentials;
 
+        return userCredentials;
     } catch (err) {
         return Promise.reject("Sign Up failed")
     }
 
+}
+
+export async function singIn(auth: Auth, email: string, password: string) {
+    try {
+        const userCredentials = await signInWithEmailAndPassword(auth, email, password);
+
+        return userCredentials;
+    } catch (err) {
+        return Promise.reject("Sign in failed")
+    }
 }
