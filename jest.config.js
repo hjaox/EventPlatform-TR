3fb458f5e65bf357c845cff1e6501a7ed6cb66d3
hjaox <hectorjana.o@gmail.com>
2024-03-24 22:00:12 +0000

    [FEAT] created scripts to run server scripts from root folder

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
+
+/* eslint no-unused-vars: ["error", { "args": "none" }] */
