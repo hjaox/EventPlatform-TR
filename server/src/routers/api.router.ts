0b519a9183ee3f09235b9440182729f621c24469
hjaox <hectorjana.o@gmail.com>
2024-03-26 00:58:06 +0000

    [FEAT] created POST /event endpoint

--- server/src/routers/api.router.ts
+++ server/src/routers/api.router.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import registerRouter from "./register.router";
 import loginRouter from "./login.router";
+import eventRouter from "./event.router";
 
 const apiRouter = express.Router();
 
 export default apiRouter
     .use("/register", registerRouter)
     .use("/login", loginRouter)
+    .use("/event", eventRouter)
