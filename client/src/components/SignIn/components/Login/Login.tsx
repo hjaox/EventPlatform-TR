08a8f170e7a5e4c3c9212a5d95783231e700119b
hjaox <hectorjana.o@gmail.com>
2024-03-26 13:30:21 +0000

    [FEAT] created SignIn component and restructured compentents of client

---
+++ client/src/components/SignIn/components/Login/Login.tsx
@@ -0,0 +1,35 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithRedirect } from "firebase/auth"
+import app from "../../../../utils/firebase/fbAuth";
+import { GoogleLoginButton } from "react-social-login-buttons";
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const navigate = useNavigate();
+    const auth = getAuth(app)
+    const provider = new GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/calendar");
+
+    function signIn() {
+        return signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result)
+                navigate("/Dashboard")
+            })
+            .catch(err => {
+                console.log(err, "err")
+            })
+    }
+
+    return <>login
+        <form id="loginForm">
+
+            <label htmlFor="emailInput">Email</label>
+            <input id="emailInput" type="text" />
+
+            <label htmlFor="passwordInput">Password</label>
+            <input type="text" id="passwordInput" />
+        </form>
+        <span>or</span>
+        <GoogleLoginButton onClick={() => signIn()} style={{width: "fit-content"}}/>
+    </>
+}
