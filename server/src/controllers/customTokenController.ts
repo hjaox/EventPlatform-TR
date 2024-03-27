cf57de8efeb9b04b893085adeeb6039615b6c5c6
hjaox <hectorjana.o@gmail.com>
2024-03-27 14:28:07 +0000

    [EDIT] changed return value of customToken endpoint

--- server/src/controllers/customTokenController.ts
+++ server/src/controllers/customTokenController.ts
@@ -1,11 +1,17 @@
+import { getCredFromCustomToken } from "../utils/firebase/fbFunctions";
 import { getCustomToken } from "../utils/firebase-admin/fbAdminFunctions";
 import express from "express";
+import auth from "../utils/firebase/fbAuth";
 
-export async function createCustomToken(req: express.Request, res: express.Response, next: express.NextFunction) {
+export async function createIdToken(req: express.Request, res: express.Response, next: express.NextFunction) {
     try {
         const customToken = await getCustomToken(req.body.uid);
-
-        return res.status(201).send({customToken});
+        if(customToken) {
+            const idToken = await getCredFromCustomToken(auth, customToken);
+            const token = await idToken?.user.getIdToken();
+            return res.status(201).send({token});
+        }
+        return Promise.reject({status: 400, msg: "Bad Request"})
     } catch(err) {
         next(err)
     }
--- server/src/routers/customToken.router.ts
+++ server/src/routers/customToken.router.ts
@@ -1,7 +1,7 @@
 import express from "express";
-import { createCustomToken } from "../controllers/customTokenController";
+import { createIdToken } from "../controllers/customTokenController";
 
 const customTokenRouter = express.Router();
 
 export default customTokenRouter
-    .post("/", createCustomToken)
+    .post("/", createIdToken)
