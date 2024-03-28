5a3e5459741a5d48e4d6acaaf5dd382ce1c3c32e
hjaox <hectorjana.o@gmail.com>
2024-03-28 09:06:19 +0000

    [STYLE] created temporary Event component layout

--- client/src/components/EventPage/Event.tsx
+++ client/src/components/EventPage/Event.tsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getEvent } from "../../utils/axios/event";
 import { TEvent } from "../../common/types";
+import Header from "../subcomponents/Header/Header";
+import Footer from "../subcomponents/Footer/Footer";
+import "../../styles/EventPage/event.scss";
 
 export default function Event() {
     const { eventId } = useParams();
@@ -16,10 +19,37 @@ export default function Event() {
         }
 
     }, []);
-    console.log(eventDetails)
+
+    function handleDateAndTime(dateStart: Date, dateEnd: Date) {
+        return <></>
+    }
+
+    function handleLocation(address: string, coords: number[]) {
+
+    }
+
     return (
         <section className="event-page">
+            <Header />
 
-        </section>
+            {
+                !eventDetails
+                    ? (
+                        <>loading</>
+                    )
+                    : (
+                        <section className="event-display">
+                            <img  className="image" src={eventDetails.images[0]} alt="pic" />
+                            <h1 className="title">{eventDetails.title}</h1>
+                            <div className="time">{handleDateAndTime(eventDetails.dateStart, eventDetails.dateEnd)}</div>
+                            <div className="location"></div>
+                            <div className="details"></div>
+                            <div className="organizer"></div>
+                        </section>
+                    )
+            }
+
+            <Footer />
+        </section >
     )
 }
