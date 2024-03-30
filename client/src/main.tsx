21c42b58a722754d58ccab908933b1492f4c8e89
hjaox <hectorjana.o@gmail.com>
2024-03-30 17:52:42 +0000

    [FEAT] created redux reducer and store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './styles/index.scss';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { persistor, store } from './utils/redux/store.tsx';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App.tsx'
 import Home from './components/Home/Home.tsx';
 import Login from './components/SignIn/components/Login/Login.tsx';
@@ -55,7 +59,12 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
+
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 )
