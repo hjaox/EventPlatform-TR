e04e0cea5a48af71cc40d790a815e83ac840a130
hjaox <hectorjana.o@gmail.com>
2024-03-30 18:29:14 +0000

    [STYLE] added some basic style to Header component and added Butterfly-Kids font

--- client/src/components/subcomponents/Header/Header.tsx
+++ client/src/components/subcomponents/Header/Header.tsx
@@ -1,9 +1,19 @@
+import { useNavigate } from "react-router-dom";
 import "../../../styles/subcomponents/Header/header.scss";
 
 export default function Header() {
+    const navigation = useNavigate();
+
     return (
         <header className="header-component">
-            <span>header</span>
+            <section className="platform-name">
+                <span className="text">The Community</span>
+            </section>
+            <section className="options">
+                <span>Login</span>
+                <span onClick={() => navigation("/Login")}>Sign Up</span>
+                <span>settings</span>
+            </section>
         </header>
     )
 }
