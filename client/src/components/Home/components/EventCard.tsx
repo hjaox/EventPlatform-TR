4a9af38b0dd7dc8272b85ab5e82e054f9875414e
hjaox <hectorjana.o@gmail.com>
2024-04-10 14:54:54 +0100

    [STYLE] created navigation for editing options of an event in Home component

--- client/src/components/Home/components/EventCard.tsx
+++ client/src/components/Home/components/EventCard.tsx
@@ -1,6 +1,8 @@
 import { TEvent } from "../../../common/types";
 import { handleDate } from "../../../utils/utils";
 import "../../../styles/Home/eventCard.scss"
+import { FaRegEdit } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
 
 export default function EventCard({ event }: { event: TEvent }) {
     console.log(event)
@@ -13,6 +15,16 @@ export default function EventCard({ event }: { event: TEvent }) {
                 <span className="eventCard-address">{event.address}</span>
                 <span className="eventCard-tag">{event.summary}</span>
             </div>
+            <div className="eventCard-option-container">
+                <div className="eventCard-icon-container">
+                    <FaRegEdit className="eventCard-option" />
+                </div>
+
+                <div className="eventCard-icon-container">
+                    <MdDeleteForever className="eventCard-option" />
+                </div>
+            </div>
+
         </>
     )
 }
