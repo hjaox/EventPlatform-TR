25b6add2748149e839ae9b1305d81b9bc39285e1
hjaox <hectorjana.o@gmail.com>
2024-04-13 20:15:35 +0100

    [FEAT] created firebase downloadImage and implemented to Event Card component

--- client/src/utils/axios/events.tsx
+++ client/src/utils/axios/events.tsx
@@ -1,6 +1,11 @@
 import instance from "./instance";
 
 export async function getAllEvents() {
-    const { data: {allEvents}} = await instance.get("./events");
-    return allEvents;
+    try {
+        const { data: { allEvents } } = await instance.get("./events");
+        if (allEvents) return allEvents;
+        return null
+    } catch (err) {
+        return null
+    }
 }
