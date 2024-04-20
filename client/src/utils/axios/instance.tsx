31d19cbd9586b57d1cc891fb5373b852edbc20a4
hjaox <hectorjana.o@gmail.com>
2024-04-21 00:58:32 +0100

    [TEST] succesfully tested live hosting app

--- client/src/utils/axios/instance.tsx
+++ client/src/utils/axios/instance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://eventplatform-tr.onrender.com"
+    baseURL: import.meta.env.VITE_axiosBaseUrl
 });
 
 export default instance;
