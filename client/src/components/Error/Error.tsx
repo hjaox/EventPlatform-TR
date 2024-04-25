d4ae349024d8188ef718614b374856e881800b52
hjaox <hectorjana.o@gmail.com>
2024-04-25 15:48:47 +0100

    [FEAT] styled Error component

--- client/src/components/Error/Error.tsx
+++ client/src/components/Error/Error.tsx
@@ -1,3 +1,15 @@
+import Footer from "../subcomponents/Footer/Footer";
+import Header from "../subcomponents/Header/Header";
+import "../../styles/Error/error.scss";
+
 export default function Error() {
-    return <>Error</>
+    return (
+    <section className="error-page">
+        <Header />
+        <section className="error-display">
+            <button>Go Home</button>
+        </section>
+        <Footer />
+    </section>
+    )
 }
