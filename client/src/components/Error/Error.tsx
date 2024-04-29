b155bd6bedbbedcb13231881dbf6e1885553cec4
hjaox <hectorjana.o@gmail.com>
2024-04-29 15:32:44 +0100

    [FEAT] added navigation from Error page to Home

--- client/src/components/Error/Error.tsx
+++ client/src/components/Error/Error.tsx
@@ -1,13 +1,16 @@
 import Footer from "../subcomponents/Footer/Footer";
 import Header from "../subcomponents/Header/Header";
 import "../../styles/Error/error.scss";
+import { useNavigate } from "react-router-dom";
 
 export default function Error() {
+    const navigate = useNavigate();
+
     return (
     <section className="error-page">
         <Header />
         <section className="error-display">
-            <button>Go Home</button>
+            <button onClick={() => navigate("/Home")}>Go Home</button>
         </section>
         <Footer />
     </section>
